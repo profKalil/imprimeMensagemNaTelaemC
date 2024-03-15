d063767cc03b3e1237667fe31b177b3e0e951ed3
profKalil <84177112+profKalil@users.noreply.github.com>
2024-03-15 11:49:13 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@imprimeMensagemNaTelaemC/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+  // Variaveis
+  // char mensagem[100] ="Ola mundo";
+  // String mensagem = "Ola mundo'; ->JAVA 
+  double numero1 = 7;
+  double numero2 = 2;
+
+  // Processos
+  double soma = pow(numero1, numero2);
+
+  // Método de saída
+  // printf("%s", mensagem);
+  printf("%.2f", soma);
+
+  return 0;
+}
